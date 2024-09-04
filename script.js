76a06e1ae831d4962c741af87e14d15cd7bb0598
Mutwassim Haider <mutwassim@gmail.com>
2024-09-05 01:13:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let stopwatch=document.querySelector(".stopwatch");
+let startButton=document.querySelector(".startButton");
+let stopButton=document.querySelector(".stopButton");
+let resetButton=document.querySelector(".resetButton");
+
+let isRunning=false;
+let sec=0;
+let min=0;
+let hr=0;
+let interval;
+function start(){
+    isRunning=true;
+    if (!isRunning){
+        interval=setInterval(() => {
+            sec++
+            if (sec===60){
+                min++;
+                sec=0;
+            }
+            if (min===60){
+                hr++;
+                min=0;
+                sec=0;
+            }
+            updateDisplay();
+        }, 1000);
+        
+    }
+}
+function stop(){
+    isRunning =false;
+    updateDisplay();
+}
+function reset(){
+
+    sec=0;
+    min=0;
+    hr=0;
+    updateDisplay();
+}
+
+
+function updateDisplay(){
+    stopwatch.innerHTML= `${hr}:${min}:${sec}`;
+}
+
+startButton.addEventListener("click",function(){start()});
+stopButton.addEventListener("click",function(){stop()});
+resetButton.addEventListener("click",function(){reset()});
+
+updateDisplay();
